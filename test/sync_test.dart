50bda4c094d63855dcd765e1d35d8c87b275cc6c
dan <iwanicki.daniel@gmail.com>
2023-03-04 21:16:20 +0100

    .

--- test/sync_test.dart
+++ test/sync_test.dart
@@ -1,8 +1,38 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:harcapp/_new/api/login_register.dart';
+import 'package:harcapp/_new/cat_page_song_book/song_management/off_song.dart';
+import 'package:harcapp/sync/synchronizer_engine.dart';
+import 'package:harcapp_core_song_widget/song_rate.dart';
+
+import 'utils.dart';
 
 void main() {
 
   test('Sync test', () async {
+
+    await initTestShaPref();
+    await initTestPaths();
+    await initTestAccountData();
+
+    enableHttpRequests();
+    enableTestConnected();
+
+    Response? response = await ApiRegLog.carefullyLogin(
+      context: MockBuildContext(),
+      email: 'a@a.com',
+      password: 'adminadmin',
+      awaitSyncFinished: true
+    );
+
+    assert(response?.statusCode == 200);
+
+    OffSong.allOfficial[0].setRate(SongRate.RATE_LIKE_1, localOnly: true);
+    OffSong.allOfficial[0].setChordShift(5, localOnly: true);
+
+    await synchronizer.post();
+
+
 
   });
 
--- test/utils.dart
+++ test/utils.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:harcapp/_common_classes/sha_pref.dart';
 import 'package:harcapp/_common_classes/storage.dart';
+import 'package:harcapp/account/account.dart';
+import 'package:mockito/mockito.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> initTestShaPref() async {
@@ -10,9 +15,41 @@ Future<void> initTestShaPref() async {
 }
 
 Future<void> initTestPaths() async {
+  TestWidgetsFlutterBinding.ensureInitialized();
   const MethodChannel channel = MethodChannel('plugins.flutter.io/path_provider');
   channel.setMockMethodCallHandler((MethodCall methodCall) async {
     return ".";
   });
   await initPaths();
 }
+
+Future<void> initTestAccountData() async {
+
+  Map secureStorageData = {};
+
+  const MethodChannel channel = MethodChannel('plugins.it_nomads.com/flutter_secure_storage');
+  channel.setMockMethodCallHandler((MethodCall methodCall) async {
+    if(methodCall.method == 'read')
+      return secureStorageData[methodCall.arguments['key']];
+    else if (methodCall.method == 'write')
+      secureStorageData[methodCall.arguments['key']] = methodCall.arguments['value'];
+  });
+  await AccountData.init();
+}
+
+class _MyHttpOverrides extends HttpOverrides {}
+void enableHttpRequests() {
+  // Setting a customer override that'll use an unmocked HTTP client
+  HttpOverrides.global = _MyHttpOverrides();
+}
+
+void enableTestConnected() {
+  const MethodChannel channel = MethodChannel('plugins.flutter.io/connectivity');
+  channel.setMockMethodCallHandler((MethodCall methodCall) async {
+    if(methodCall.method == 'check')
+      return 'wifi';
+  });
+
+}
+
+class MockBuildContext extends Mock implements BuildContext {}
